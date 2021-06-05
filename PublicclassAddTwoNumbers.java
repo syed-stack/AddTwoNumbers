a6fd52c403779c6843aed42e354fcbb8d3ee7273
syed-stack <84707016+syed-stack@users.noreply.github.com>
2021-06-05 19:35:51 +0530

    AddTwoNumbers

---
+++ PublicclassAddTwoNumbers.java
@@ -0,0 +1,13 @@
+package first;
+
+public class PublicclassAddTwoNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x =50;
+		int y =20;
+		System.out.println("sum of the two numbers:"+(x+y));
+
+	}
+
+}
